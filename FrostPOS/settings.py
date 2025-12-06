1ba0e108fce427508ced176727a240a8cfc2d74a
peteandremanuel0@gmail.com <pete>
2025-12-06 22:10:31 +0800

    secondupdatedsettings

--- FrostPOS/settings.py
+++ FrostPOS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aitzk!=uz^f!=e6_l=6z6r4$jb!-@w7y!w2!-yp)i@ccog*jgi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://frostypos.onrender.com']
 
 
 # Application definition
